2ffc03fef7843a4c826956bf0ef6fa2079a0b1b8
你的Aquila <Aquila@9li.life>
2026-05-12 07:18:12 +0800

    build(vite): 显式指定 target=es2020 + 关闭 sourcemap，减小 release 包体积

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,11 @@ export default defineConfig(async () => ({
     },
   },
 
+  build: {
+    target: 'es2020',
+    sourcemap: false,
+  },
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
